65d4d832c07db92c56096ec68135f6d2c482bf16
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2025-12-29 17:49:21 -0500

    processing into direction and number

--- day1/day1_1.js
+++ day1/day1_1.js
@@ -3,10 +3,16 @@
 // Retrieved 2025-12-29, License - CC BY-SA 4.0
 import { readFileSync } from "fs";
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
-const inputFile = args[0]
+const inputFile = args[0];
 
-const input = readFileSync(inputFile, { encoding: "utf8" });
+const rawInput = readFileSync(inputFile, { encoding: "utf8" });
 
-console.log(input.split("\r\n"));
+const input = rawInput.split("\r\n");
+
+const output = input
+  .map((itemStr) => [itemStr[0], itemStr.slice(1)])
+  .map(([dir, numStr]) => [dir, parseInt(numStr, 10)]);
+
+console.log(output);
